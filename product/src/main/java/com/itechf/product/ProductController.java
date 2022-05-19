fa03e7b3d62b71b51df1a47d2105a20d3a131374
Kaunain Ahmad <kaunain.ahmad2000@gmail.com>
2022-05-19 17:41:09 +0530

    Crud Added

--- product/src/main/java/com/itechf/product/ProductController.java
+++ product/src/main/java/com/itechf/product/ProductController.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,19 +23,40 @@ public class ProductController {
         return pi.get();
     }
 
+    @DeleteMapping("/product/{id}")
+    public Boolean deleteProductItem(@PathVariable Integer id) {
+        productItemRepository.deleteById(id);
+        return true;
+    }
+
     @GetMapping("/all")
     public List<ProductItem> getProductItems() {
         return productItemRepository.findAll();
     }
 
-    @GetMapping("/init")
-    public List<ProductItem> saveInit() {
+    @PostMapping("/save")
+    public ProductItem saveProductItems(@RequestBody ProductItem productItem) {
+        return productItemRepository.save(productItem);
+    }
+
+    @PostMapping("/update")
+    public ProductItem updateProductItems(@RequestBody ProductItem productItem) {
+        return productItemRepository.save(productItem);
+    }
+
+    @PostMapping("/init")
+    public Boolean saveInit(@RequestBody String key) {
+        Boolean flag = false;
         List<ProductItem> productList = List.of(
             new ProductItem(1, "Book", 1, "Some Book"), 
             new ProductItem(2, "Table", 2, "Simple Table"),
             new ProductItem(3, "Chair", 3, "Sitting Chair")
-        );
-        return productItemRepository.saveAll(productList);
+            );
+        if("123123".equals(key)){
+            productItemRepository.saveAll(productList);
+            flag = true;
+        }
+        return flag;
     }
 
     @GetMapping("/")
